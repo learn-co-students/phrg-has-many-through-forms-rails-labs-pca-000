085fc17e839a02322586b8730ab762ba72a791fd
Darren DCosta <ddcosta@gmail.com>
2019-05-22 20:03:21 -0400

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,8 @@
 
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(comment_params)
+    post = Post.find(params["post_id"])
+    comment = post.comments.create(comment_params)
     redirect_to comment.post
   end
 
